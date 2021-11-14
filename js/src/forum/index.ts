cc494d980ae33585aa3dbcfff11e287c74edc659
Rafał Całka <calka.rafal@gmail.com>
2021-11-14 21:06:33 +0100

    Cleaner code

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,13 +1,11 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import LogInModal from 'flarum/forum/components/LogInModal';
-import updateCSRF from './utils/updateCSRF';
 import SignUpModal from 'flarum/forum/components/SignUpModal';
+import updateCSRF from './utils/updateCSRF';
 
 app.initializers.add('acpl-lscache', () => {
   [LogInModal, SignUpModal].forEach((Component) => {
-    extend(Component.prototype, 'oninit', () => {
-      updateCSRF();
-    });
+    extend(Component.prototype, 'oninit', updateCSRF);
   });
 });
