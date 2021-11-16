15fd7ff4ccd2242b966538d531e5103df2063fe1
Rafał Całka <calka.rafal@gmail.com>
2021-11-16 18:51:31 +0100

    Update CSRF on every modal init

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,12 +1,9 @@
 import app from 'flarum/forum/app';
-// import { extend } from 'flarum/common/extend';
-// import LogInModal from 'flarum/forum/components/LogInModal';
-// import SignUpModal from 'flarum/forum/components/SignUpModal';
+import { extend } from 'flarum/common/extend';
+import Modal from 'flarum/common/components/Modal';
 import updateCSRF from './utils/updateCSRF';
 
 app.initializers.add('acpl-lscache', () => {
-  // [LogInModal, SignUpModal].forEach((Component) => {
-  //   extend(Component.prototype, 'oninit', updateCSRF);
-  // });
-  updateCSRF();
+  // We extend each modal to also include those added by external extensions
+  extend(Modal.prototype, 'oninit', updateCSRF);
 });
--- js/src/forum/utils/updateCSRF.ts
+++ js/src/forum/utils/updateCSRF.ts
@@ -1,6 +1,10 @@
 import app from 'flarum/forum/app';
 
 export default async () => {
+  if (app.session.user) {
+    return;
+  }
+
   // We do not use `app.request` here because that function returns only the body and we need headers
   const res = await fetch(`${app.forum.attribute('apiUrl')}/lscache-csrf`);
   const token = res.headers.get('X-CSRF-Token');
