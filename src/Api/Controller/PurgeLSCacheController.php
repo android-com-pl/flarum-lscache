10709798c04817d6db3dc52a1203d1b6e77dbe88
Rafał Całka <calka.rafal@gmail.com>
2021-11-16 06:23:51 +0100

    Fix autoload

--- src/Api/Controller/PurgeLSCacheController.php
+++ src/Api/Controller/PurgeLSCacheController.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class PurgeLsCacheController implements RequestHandlerInterface
+class PurgeLSCacheController implements RequestHandlerInterface
 {
     /**
      * @throws PermissionDeniedException
