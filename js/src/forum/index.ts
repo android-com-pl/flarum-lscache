eb34e731080e252bcbac19f80747b1318ed6d8ca
Rafał Całka <calka.rafal@gmail.com>
2021-11-16 18:38:06 +0100

    Always update CSRF

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,11 +1,12 @@
 import app from 'flarum/forum/app';
-import { extend } from 'flarum/common/extend';
-import LogInModal from 'flarum/forum/components/LogInModal';
-import SignUpModal from 'flarum/forum/components/SignUpModal';
+// import { extend } from 'flarum/common/extend';
+// import LogInModal from 'flarum/forum/components/LogInModal';
+// import SignUpModal from 'flarum/forum/components/SignUpModal';
 import updateCSRF from './utils/updateCSRF';
 
 app.initializers.add('acpl-lscache', () => {
-  [LogInModal, SignUpModal].forEach((Component) => {
-    extend(Component.prototype, 'oninit', updateCSRF);
-  });
+  // [LogInModal, SignUpModal].forEach((Component) => {
+  //   extend(Component.prototype, 'oninit', updateCSRF);
+  // });
+  updateCSRF();
 });
