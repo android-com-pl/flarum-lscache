5fa7fb56d8ffbfee243275d5e6c60b4252375bbc
Rafał Całka <calka.rafal@gmail.com>
2023-05-16 22:14:55 +0200

    fix: Restore old migration file

---
+++ migrations/2021_12_11_000000_enable_clearing_cache_listener.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This migration file is required for backward compatibility purposes.
+ * It was involved in a previous version of this extension, and its absence might cause issues when uninstalling.
+ * The 'up' and 'down' keys are left empty intentionally.
+ * DO NOT REMOVE THIS FILE.
+ */
+return [
+    'up' => function () {
+    },
+    'down' => function () {
+    }
+];
