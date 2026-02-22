15fc496627565ce26a9e52764ff7b851108fa088
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-02-22 14:04:10 +0100

    perf: exclude hidden posts and private discussions from user cache purge

--- src/Listener/UserEventSubscriber.php
+++ src/Listener/UserEventSubscriber.php
@@ -24,6 +24,8 @@ class UserEventSubscriber extends AbstractCachePurgeSubscriber
             'posts',
             'discussions',
             ...$event->user->posts()
+                ->whereNull('hidden_at')
+                ->whereHas('discussion', fn ($query) => $query->whereNull('hidden_at')->where('is_private', false))
                 ->distinct()
                 ->pluck('discussion_id')
                 ->map(fn ($id) => "discussion_$id")
